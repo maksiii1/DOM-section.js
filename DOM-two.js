37959919bb1997e339d4f01ed24c15eeeef39393
Maksin Spizhuk <indcheat@mail.ru>
2025-04-02 16:06:40 +0300

    added dom-two.js

---
+++ DOM-two.js
@@ -0,0 +1,7 @@
+const showText = () => {
+  let input = (document.getElementById("text").placeholder = "test@email.ru");
+};
+
+const deleteText = () => {
+  let input = (document.getElementById("text").placeholder = "");
+};
