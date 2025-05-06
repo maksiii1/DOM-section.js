5e414a2ef49a16afcbe31a6a187cc3e9ae683039
Maksin Spizhuk <indcheat@mail.ru>
2025-05-06 23:20:49 +0300

    fix DOM-five.js

--- DOM-five.js
+++ DOM-five.js
@@ -1,10 +1,11 @@
 let buttonOneSesction5 = document.getElementById("section-5__button");
 let buttonTwoSesction5 = document.getElementById("section-5__button-2");
+let inputSection5 = document.getElementById("section-5__input-1");
 
 buttonOneSesction5.addEventListener("click", () => {
-  document.getElementById("section-5__input-1").setAttribute("disabled", "");
+  inputSection5.setAttribute("disabled", "");
 });
 
 buttonTwoSesction5.addEventListener("click", () => {
-  document.getElementById("section-5__input-1").removeAttribute("disabled");
+  inputSection5.removeAttribute("disabled");
 });
