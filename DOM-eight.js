81f6492051a9659e7ce35f5e7ab1f73bab3daf71
Maksin Spizhuk <indcheat@mail.ru>
2025-05-08 22:08:34 +0300

    fix dom-eight.js

--- DOM-eight.js
+++ DOM-eight.js
@@ -1,21 +1,23 @@
-let container = document.getElementById("squaresGroup");
+const container = document.querySelector("#squaresGroup");
 let activeSquare = null;
 
 for (let index = 0; index < 4; index++) {
-  let containerSquare = document.createElement("div");
-  containerSquare.className = "square3";
-  container.appendChild(containerSquare);
+  let square = document.createElement("div");
+  square.className = "square3";
+  container.appendChild(square);
 }
 
 container.addEventListener("click", (event) => {
   let target = event.target;
 
-  if (target.classList.contains("square3")) {
-    if (activeSquare) {
-      activeSquare.style.backgroundColor = "rgb(255, 0, 0)";
-    }
-
-    target.style.backgroundColor = "rgb(0, 248, 41)";
-    activeSquare = target;
+  if (!target.classList.contains("square3")) {
+    return;
   }
+
+  if (activeSquare) {
+    activeSquare.classList.remove("section-8__switchColor");
+  }
+
+  target.classList.add("section-8__switchColor");
+  activeSquare = target;
 });
